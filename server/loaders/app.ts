7bf8823d962a38b06db83f423cca746ca9bfc748
Anish Dangol <dangol.anish001@gmail.com>
2025-06-21 23:48:55 +0545

    update: configured cors for local machine

--- server/loaders/app.ts
+++ server/loaders/app.ts
@@ -5,7 +5,13 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // Allow all origins
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 

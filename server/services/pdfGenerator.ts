df7a804c061ee8202e3df15771ddf45651849498
Anish Dangol <dangol.anish001@gmail.com>
2025-06-22 01:03:49 +0545

    pupeteer browser install

--- server/services/pdfGenerator.ts
+++ server/services/pdfGenerator.ts
@@ -2,6 +2,7 @@ import puppeteer from "puppeteer";
 
 export async function htmlToPdf(html: string): Promise<Buffer> {
   const browser = await puppeteer.launch({
+    executablePath: "/usr/bin/google-chrome",
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
   const page = await browser.newPage();
